827a26d043562e1eedba1cd8bff93dfe55fcb5f3
ken01235 <kenkenrc08@gmail.com>
2021-04-07 03:15:36 -0700

    modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/.profile-ld 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.bin 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.elf 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.map 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.map.old 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.csv 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.html 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.json 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/main.o 	new file:   FFT.py 	modified:   main.cpp

---
+++ FFT.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
+import os
+
+Fs = 1000.0
+Ts = 1.0/Fs
+t = np.arange(0,0.5,Ts)
+y = np.arange(0,0.5,Ts)
+
+n = len(y)
+k = np.arange(n)
+T = n/Fs
+frq = k/T # a vector of frequencies; two sides frequency range
+frq = frq[range(int(n/2))] # one side frequency range
+
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev)
+for x in range(0, 500):
+    line = s.readline()
+    y[x] = float(line)
+
+Y = np.fft.fft(y)/n*2 # fft computing and normalization
+Y = Y[range(int(n/2))] # remove the conjugate frequency parts
+
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(t,y)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Amplitude')
+ax[0].set_ylim([0, 1.0])
+ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].set_xlabel('Freq (Hz)')
+ax[1].set_ylabel('|Y(freq)|')
+ax[1].set_xlim([0, 200])
+plt.show()
+s.close()
--- main.cpp
+++ main.cpp
@@ -5,19 +5,24 @@ DigitalIn bottom1(D8);
 DigitalIn bottom2(D9);
 DigitalIn bottom3(D10);
 AnalogOut aout(PA_5);
+AnalogIn ain(A0);
 
+int num = 0;
+uint16_t output[240] = {0};
 uLCD_4DGL uLCD(D1, D0, D2);
 
 void print_num(int);
-void generate_output(uint16_t[], int);
+void generate_output(void);
+void sample(void);
 
 int main()
 {
     int bt1 = 0, bt2 = 0, bt3 = 0;
-    int num = 0;
-    int selected_num = 0;
-    int i = 0;
-    uint16_t output[240] = {0};
+
+    EventQueue queue(2 * EVENTS_EVENT_SIZE);
+    Thread t1;
+    Thread t2;
+
     uLCD.background_color(BLACK);
     uLCD.color(GREEN);
     uLCD.textbackground_color(BLACK);
@@ -25,7 +30,7 @@ int main()
     uLCD.text_height(3);
     uLCD.locate(0, 2);
     uLCD.printf(" 1/8 ");
-    while (1) {
+    while(1) {
         if (!bt1 && bottom1) {
             num = (num + 1) % 4;
             uLCD.locate(0, 2);
@@ -47,43 +52,16 @@ int main()
         }
 
         if (!bt3 && bottom3) {
-            selected_num = num;
-            generate_output(output, selected_num);
             uLCD.locate(0, 2);
             uLCD.color(RED);
             print_num(num);
             uLCD.color(GREEN);
             bt3 = bottom3;
+            break;
         } else if (bt3 && !bottom3) {
             bt3 = bottom3;
         }
-        aout.write_u16(output[i]);
-
-        i = (i + 1) % 240;
-        ThisThread::sleep_for(1ms);
     }
-}
-
-void print_num(int num) {
-    switch (num) {
-        case 0:
-            uLCD.printf(" 1/8 ");
-            break;
-        case 1:
-            uLCD.printf(" 1/4 ");
-            break;
-        case 2:
-            uLCD.printf(" 1/2 ");
-            break;
-        case 3:
-            uLCD.printf("  1  ");
-            break;
-        default:
-            break;
-    }
-}
-
-void generate_output(uint16_t output[240], int num) {
     if (num ==  3) {
         for (int i = 0; i < 80; ++i) {
             output[i] = (65535 * 3 / 3.3 * i / 80);
@@ -124,6 +102,139 @@ void generate_output(uint16_t output[240], int num) {
         for (int i = 0; i < 10; ++i) {
             output[i + 230] = (65535 * 3 / 3.3 * (240 - i - 230) / 10);
         }
+    }
+    // queue.call(generate_output);
+    // queue.dispatch();
+    if (num ==  3) {
+        for (int i = 0; i < 80; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 80);
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 80] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 160] = (65535 * 3 / 3.3 * (240 - i - 160) / 80);
+        }
+    } else if (num == 2) {
+        for (int i = 0; i < 40; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 40);
+        }
+        for (int i = 0; i < 160; ++i) {
+            output[i + 40] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 40; ++i) {
+            output[i + 200] = (65535 * 3 / 3.3 * (240 - i - 200) / 40);
+        }
+    } else if (num == 1) {
+        for (int i = 0; i < 20; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 20);
+        }
+        for (int i = 0; i < 200; ++i) {
+            output[i + 20] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 20; ++i) {
+            output[i + 220] = (65535 * 3 / 3.3 * (240 - i - 220) / 20);
+        }
+    } else if (num == 0) {
+        for (int i = 0; i < 10; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 10);
+        }
+        for (int i = 0; i < 220; ++i) {
+            output[i + 10] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 10; ++i) {
+            output[i + 230] = (65535 * 3 / 3.3 * (240 - i - 230) / 10);
+        }
+    }
+    int i = 0;
+    int j = 0;
+    float answer[500] = {0};
+    while (1) {
+        aout.write_u16(output[i]);
+        i = (i + 1) % 240;
+        if (j < 500) {
+            answer[j] = ain;
+            j++;
+        } else if (j == 500) {
+            for (int k = 0; k < 500; ++k) {
+                printf("%f\n", answer[k]);
+            }
+            j++;
+        }
+        ThisThread::sleep_for(1ms);
+    }
+}
+
+void print_num(int num) {
+    switch (num) {
+    case 0:
+        uLCD.printf(" 1/8 ");
+        break;
+    case 1:
+        uLCD.printf(" 1/4 ");
+        break;
+    case 2:
+        uLCD.printf(" 1/2 ");
+        break;
+    case 3:
+        uLCD.printf("  1  ");
+        break;
+    default:
+        break;
+    }
+}
+
+void generate_output() {
+    if (num ==  3) {
+        for (int i = 0; i < 80; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 80);
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 80] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 160] = (65535 * 3 / 3.3 * (240 - i - 160) / 80);
+        }
+    } else if (num == 2) {
+        for (int i = 0; i < 40; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 40);
+        }
+        for (int i = 0; i < 160; ++i) {
+            output[i + 40] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 40; ++i) {
+            output[i + 200] = (65535 * 3 / 3.3 * (240 - i - 200) / 40);
+        }
+    } else if (num == 1) {
+        for (int i = 0; i < 20; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 20);
+        }
+        for (int i = 0; i < 200; ++i) {
+            output[i + 20] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 20; ++i) {
+            output[i + 220] = (65535 * 3 / 3.3 * (240 - i - 220) / 20);
+        }
+    } else if (num == 0) {
+        for (int i = 0; i < 10; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 10);
+        }
+        for (int i = 0; i < 220; ++i) {
+            output[i + 10] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 10; ++i) {
+            output[i + 230] = (65535 * 3 / 3.3 * (240 - i - 230) / 10);
+        }
+    }
+}
+
+void sample() {
+    float answer[500] = {0};
+    for (int i = 0; i < 500; ++i) {
+        answer[i] = ain;
+    }
+    for (int i = 0; i < 500; ++i) {
+        printf("%f\n", answer[i]);
     }
 }
 
