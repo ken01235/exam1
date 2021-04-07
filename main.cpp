fa7c8a8dacb186abe892a77743854960e7463aa6
ken01235 <kenkenrc08@gmail.com>
2021-04-07 01:56:09 -0700

    modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/.profile-ld 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.bin 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.elf 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.map 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1.map.old 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.csv 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.html 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/exam1_map.json 	modified:   BUILD/B_L4S5I_IOT01A/GCC_ARM/main.o 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@
 DigitalIn bottom1(D8);
 DigitalIn bottom2(D9);
 DigitalIn bottom3(D10);
+AnalogOut aout(PA_5);
 
 uLCD_4DGL uLCD(D1, D0, D2);
 
-void print_num(int num);
+void print_num(int);
+void generate_output(uint16_t[], int);
 
 int main()
 {
     int bt1 = 0, bt2 = 0, bt3 = 0;
     int num = 0;
     int selected_num = 0;
+    int i = 0;
+    uint16_t output[240] = {0};
     uLCD.background_color(BLACK);
     uLCD.color(GREEN);
     uLCD.textbackground_color(BLACK);
@@ -44,6 +48,7 @@ int main()
 
         if (!bt3 && bottom3) {
             selected_num = num;
+            generate_output(output, selected_num);
             uLCD.locate(0, 2);
             uLCD.color(RED);
             print_num(num);
@@ -52,8 +57,10 @@ int main()
         } else if (bt3 && !bottom3) {
             bt3 = bottom3;
         }
+        aout.write_u16(output[i]);
 
-        ThisThread::sleep_for(100ms);
+        i = (i + 1) % 240;
+        ThisThread::sleep_for(1ms);
     }
 }
 
@@ -73,6 +80,50 @@ void print_num(int num) {
             break;
         default:
             break;
+    }
+}
+
+void generate_output(uint16_t output[240], int num) {
+    if (num ==  3) {
+        for (int i = 0; i < 80; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 80);
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 80] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 80; ++i) {
+            output[i + 160] = (65535 * 3 / 3.3 * (240 - i - 160) / 80);
+        }
+    } else if (num == 2) {
+        for (int i = 0; i < 40; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 40);
+        }
+        for (int i = 0; i < 160; ++i) {
+            output[i + 40] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 40; ++i) {
+            output[i + 200] = (65535 * 3 / 3.3 * (240 - i - 200) / 40);
+        }
+    } else if (num == 1) {
+        for (int i = 0; i < 20; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 20);
+        }
+        for (int i = 0; i < 200; ++i) {
+            output[i + 20] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 20; ++i) {
+            output[i + 220] = (65535 * 3 / 3.3 * (240 - i - 220) / 20);
+        }
+    } else if (num == 0) {
+        for (int i = 0; i < 10; ++i) {
+            output[i] = (65535 * 3 / 3.3 * i / 10);
+        }
+        for (int i = 0; i < 220; ++i) {
+            output[i + 10] = 65535 * 3 / 3.3;
+        }
+        for (int i = 0; i < 10; ++i) {
+            output[i + 230] = (65535 * 3 / 3.3 * (240 - i - 230) / 10);
+        }
     }
 }
 
